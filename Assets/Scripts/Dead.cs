4a25fff71c79c80a23683418db533b047b312643
franciscoSecchiTarres <82807613+FranSecchi@users.noreply.github.com>
2023-10-20 15:30:57 +0200

    Enemy drops item on death now

--- Assets/Scripts/Dead.cs
+++ Assets/Scripts/Dead.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Dead : FSMState
 {
+    public List<GameObject> items;
     private bool deadAlready;
     protected override void Initialize()
     {
@@ -19,9 +20,17 @@ public class Dead : FSMState
         if (!deadAlready)
         {
             Destroy(agent);
+            Destroy(transform.Find("Life_Bar").gameObject);
             gameObject.AddComponent<Rigidbody>().useGravity = true;
+            StartCoroutine(DropItems());
             deadAlready = true;
         }
     }
 
+    private IEnumerator DropItems()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GameObject item = items[Random.Range(0, items.Count)];
+        Instantiate(item, transform.position, Quaternion.identity);
+    }
 }
