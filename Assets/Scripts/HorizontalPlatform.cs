1c2fc1f0184dc3d609b7a66fe63f262e5d4ce338
franciscoSecchiTarres <82807613+FranSecchi@users.noreply.github.com>
2023-10-22 17:17:42 +0200

    Horizontal platform

--- Assets/Scripts/HorizontalPlatform.cs
+++ Assets/Scripts/HorizontalPlatform.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class HorizontalPlatform : MonoBehaviour
 {
     public List<Transform> wayPoints;
-
+    public float speed;
+    public bool flag;
+    private int nextPoint = 0;
+    private int direction = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = wayPoints[0].position;
+        nextPoint = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(nextPoint);
+        if (transform.position != wayPoints[nextPoint].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, wayPoints[nextPoint].position, speed * Time.deltaTime);
+        }
+        else
+        {
+            nextPoint += direction;
+            if(nextPoint == wayPoints.Count || nextPoint < 0)
+            {
+                if (flag)
+                {
+                    direction *= -1;
+                    nextPoint += direction;
+                }
+                else
+                {
+                    nextPoint = 0;
+                }
+            }
+        }
     }
 }
